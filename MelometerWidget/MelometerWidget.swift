59ffea10da12d56abb6ea0bf203da98cc650280c
Seungwan98 <sinna3210@gmail.com>
2025-04-22 18:45:09 +0900

    (#104) feat: Widget image 관련 이슈 수정

--- MelometerWidget/MelometerWidget.swift
+++ MelometerWidget/MelometerWidget.swift
@@ -8,24 +8,32 @@
 import WidgetKit
 import SwiftUI
 
+func convertDate(date: Date) -> String {
+    let date = Date()
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy.MM.dd (E)" // 원하는 형식
+    let result = formatter.string(from: date)
+    return result
+}
+
 struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), emoji: "😀")
+        SimpleEntry(date: .now, startDate: convertDate(date: .now), couplesName: "김유미", dDay: 222)
     }
 
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), emoji: "😀")
+        let entry = SimpleEntry(date: .now, startDate: convertDate(date: .now), couplesName: "김유미",  dDay: 222)
         completion(entry)
     }
 
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+    func getTimeline(in context: Context, completion: @escaping (Timeline<SimpleEntry>) -> ())  {
         var entries: [SimpleEntry] = []
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
             let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, emoji: "😀")
+            let entry = SimpleEntry(date: .now, startDate: convertDate(date: .now), couplesName: "김유미",  dDay: 222)
             entries.append(entry)
         }
 
@@ -39,21 +47,40 @@ struct Provider: TimelineProvider {
 }
 
 struct SimpleEntry: TimelineEntry {
-    let date: Date
-    let emoji: String
+    var date: Date
+    
+    let startDate: String
+    let couplesName: String
+    let dDay: Int
 }
 
 struct MelometerWidgetEntryView : View {
     var entry: Provider.Entry
+    
 
     var body: some View {
         VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
-
-            Text("Emoji:")
-            Text(entry.emoji)
+            HStack(spacing: 6) {
+                Image("DdayIcon")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit).frame(width: 24, height: 24)
+                
+                VStack(alignment: .leading) {
+                    Text(entry.couplesName).font(.system(size: 10))
+                    Text("\(entry.startDate)").font(.system(size: 10))
+                }
+                
+                Spacer()
+            }.frame(width: .infinity ,height: 40)
+ 
+            Spacer()
+            HStack(alignment: .center, spacing: 1) {
+                Spacer()
+                Text("\(entry.dDay) 일").font(.title)
+          
+            }
         }
+       
     }
 }
 
@@ -76,9 +103,9 @@ struct MelometerWidget: Widget {
     }
 }
 
-//#Preview(as: .systemSmall) {
-//    MelometerWidget()
-//} timeline: {
-//    SimpleEntry(date: .now, emoji: "😀")
-//    SimpleEntry(date: .now, emoji: "🤩")
-//}
+#Preview(as: .systemSmall) {
+    MelometerWidget()
+} timeline: {
+    SimpleEntry(date: .now, startDate: convertDate(date: .now), couplesName: "김유미", dDay: 222)
+    
+}
