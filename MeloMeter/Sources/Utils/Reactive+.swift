b149ddd1fcea04c701a9d9bcc34a7f343c8b3e08
Seungwan98 <sinna3210@gmail.com>
2025-05-26 11:23:03 +0900

    add: add Reactive Extensions

---
+++ MeloMeter/Sources/Utils/Reactive+.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  MeloMeter
+//
+//  Created by 양승완 on 5/23/25.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+
+extension Reactive where Base: UITextField {
+    var textOrEmpty: ControlProperty<String> {
+        return  self.text.orEmpty
+    }
+}
+
+
